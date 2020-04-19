d46ae62b66df8e894b9329f57880eeea65a0002f
Andy Nicholson <andrew@anicholson.net>
2020-04-19 22:44:09 +1000

    Add specs for extra simple matchers

--- spec/version_tools_spec.cr
+++ spec/version_tools_spec.cr
@@ -29,12 +29,70 @@ def greater_test
   end
 end
 
-describe "version checker" do
-  it "lesser_test" do
-    lesser_test.should eq("Matches lesser")
-  end
+def equal_test
+  test_checker("0.1.0") do
+    lesser do
+      puts "this won't print"
+      "is not lesser"
+    end
 
-  it "greater_test" do
-    greater_test.should eq("Matches greater")
+    greater do
+      puts "this won't print"
+      "is not greater"
+    end
+
+    equal do
+      "Matches equal"
+    end
+  end
+end
+
+def lesser_or_equal_test
+  test_checker("0.1.0") do
+    lesser do
+      puts "this won't print"
+      "is not lesser"
+    end
+
+    lesser_or_equal do
+      "Matches lesser_or_equal"
+    end
+  end
+end
+
+def greater_or_equal_test
+  test_checker("0.1.0") do
+    greater do
+      puts "this won't print"
+      "is not greater"
+    end
+
+    greater_or_equal do
+      "Matches greater_or_equal"
+    end
+  end
+end
+
+describe "version checker" do
+  describe "simple matchers" do
+    it "lesser_test" do
+      lesser_test.should eq("Matches lesser")
+    end
+
+    it "greater_test" do
+      greater_test.should eq("Matches greater")
+    end
+
+    it "equal_test" do
+      equal_test.should eq("Matches equal")
+    end
+
+    it "lesser_or_equal_test" do
+      lesser_or_equal_test.should eq("Matches lesser_or_equal")
+    end
+
+    it "greater_or_equal_test" do
+      greater_or_equal_test.should eq("Matches greater_or_equal")
+    end
   end
 end
